bac0ea706847ae4f62d06d385b885a55918ffcda
Matt Owens <Wulfy65@gmail.com>
2017-07-26 21:25:54 -0400

    Signup Validation

--- config/passport.js
+++ config/passport.js
@@ -17,7 +17,16 @@ passport.use('local.signup', new LocalStrategy({
   passwordField: 'password',
   passReqToCallback: true
 }, function(req, email, password, done){
-  console.log('asdf');
+  req.checkBody('email', 'Invalid email').notEmpty().isEmail();
+  req.checkBody('password', 'Password must be at least 6 characters').notEmpty().isLength({min:6});
+  const errors = req.validationErrors();
+  if(errors){
+    const messages = [];
+    errors.forEach(function(error){
+      messages.push(error.msg);
+    });
+    return done(null, false, req.flash('error', messages));
+  }
   User.findOne({'email': email}, function(err, user){
     if(err){
       return done(err);
